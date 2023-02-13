cbd8e4474c2d0db22174efa9667fd1054a1ff1b1
廖文禧 <1219585136@qq.com>
2023-02-13 18:12:15 +0800

    refector: 将StaticQuery组件改为使用useStaticQuery hook

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { rhythm } from '../utils/typography'
-import { StaticQuery, graphql } from 'gatsby'
+import { useStaticQuery, graphql } from 'gatsby'
 
 const query = graphql`
   query {
@@ -15,49 +15,38 @@ const query = graphql`
   }
 `
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <StaticQuery
-        query={query}
-        render={
-          data => {
-            const linkList = Object.entries(data.site.siteMetadata.social).map(item => ({
-              label: item[0],
-              url: item[1]
-            }))
+const Footer = () => {
+  const data = useStaticQuery(query)
+  const linkList = Object.entries(data.site.siteMetadata.social).map(item => ({
+    label: item[0],
+    url: item[1]
+  }))
 
-            return (
-              <footer
-                style={{
-                  marginTop: rhythm(2.5),
-                  paddingTop: rhythm(1),
-                }}
-              >
-                {linkList.reduce((pre, item, index) => {
-                  if (index !== 0) {
-                    pre.push(<span key={'point' + index}>&nbsp;&bull;&nbsp;</span>)
-                  }
-                  pre.push(
-                    <a
-                      href={item.url}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      key={index}
-                    >
-                      {item.label}
-                    </a>
-                  )
-                  return pre
-                }, [])}
-              </footer>
-            )
-          }
+  return (
+    <footer
+      style={{
+        marginTop: rhythm(2.5),
+        paddingTop: rhythm(1),
+      }}
+    >
+      {linkList.reduce((pre, item, index) => {
+        if (index !== 0) {
+          pre.push(<span key={'point' + index}>&nbsp;&bull;&nbsp;</span>)
         }
-      >
-      </StaticQuery>
-    )
-  }
+        pre.push(
+          <a
+            href={item.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            key={index}
+          >
+            {item.label}
+          </a>
+        )
+        return pre
+      }, [])}
+    </footer>
+  )
 }
 
 export default Footer
