ee81e6b69ecb62a4b3148face5bcf9fde19c2e1d
廖文禧 <1219585136@qq.com>
2023-02-22 16:30:53 +0800

    feat(components/Bio.tsx): 修改描述

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -2,23 +2,29 @@ import React from 'react'
 import profilePic from '../images/profile-pic.png'
 import { rhythm } from '../utils/typography'
 
+const bioContainerStyle = {
+    display: 'flex'
+}
+
+const avatarStyle = {
+    marginRight: rhythm(1 / 2),
+    marginBottom: 0,
+    width: rhythm(2),
+    height: rhythm(2),
+    borderRadius: '50%'
+}
+
 const Bio: React.FC = () => {
     return (
-        <div style={{ display: 'flex' }}>
+        <div style={bioContainerStyle}>
             <img
                 src={profilePic}
                 alt="elvis's avatar"
-                style={{
-                    marginRight: rhythm(1 / 2),
-                    marginBottom: 0,
-                    width: rhythm(2),
-                    height: rhythm(2),
-                    borderRadius: '50%'
-                }}
+                style={avatarStyle}
             />
             <div>
-                Elvis的个人博客.<br />
-                好好学习，天天向上
+                Hello, I'm Elvis.<br />
+                A front-end engineer.
             </div>
         </div>
     )
