18055b21791795d2ec2bec17d34d9ef4e80628ad
廖文禧 <1219585136@qq.com>
2023-03-06 09:28:04 +0800

    feat(pages/page404.tsx): 添加404页面

---
+++ src/pages/404.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Page404: React.FC = () => {
+    return (
+        <span
+            style={{
+                color: 'var(--textNormal)'
+            }}
+        >
+            page not found
+        </span>
+    )
+}
+
+export default Page404
