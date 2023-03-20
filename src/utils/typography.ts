a99e84c1f5537efd103330c3301874bb6ea0f176
liaowenxi <1219585136@qq.com>
2023-03-21 00:55:19 +0800

    feat(utils/typography.ts): 标题样式调整

--- src/utils/typography.ts
+++ src/utils/typography.ts
@@ -9,35 +9,42 @@ Wordpress2016.overrideThemeStyles = () => ({
     },
     'h1, h2, h3, h4, h5, h6': {
         margin: 0,
-        textTransform: 'unset'
+        textTransform: 'unset',
+        paddingBottom: '1.75rem'
     },
     'a.anchor.before': {
         top: '2rem'
     },
     'h1': {
         paddingTop: '3.5rem',
-        paddingBottom: '1.75rem'
     },
     'h1 a.anchor.before': {
         top: '3.5rem'
     },
     'h2': {
-        paddingTop: '3rem',
-        paddingBottom: '1.75rem'
+        paddingTop: '3rem'
     },
     'h2 a.anchor.before': {
         top: '3rem'
     },
     'h3': {
-        paddingTop: '2.5rem',
-        paddingBottom: '1.75rem'
+        paddingTop: '2.5rem'
     },
     'h3 a.anchor.before': {
         top: '2.5rem'
     },
     'h4, h5, h6': {
-        paddingTop: '2rem',
-        paddingBottom: '1.75rem',
+        paddingTop: '2rem'
+    },
+    [`
+        h1 + h1, h1 + h2, h1 + h3, h1 + h4, h1 + h5, h1 + h6,
+        h2 + h1, h2 + h2, h2 + h3, h2 + h4, h2 + h5, h2 + h6,
+        h3 + h1, h3 + h2, h3 + h3, h3 + h4, h3 + h5, h3 + h6,
+        h4 + h1, h4 + h2, h4 + h3, h4 + h4, h4 + h5, h4 + h6,
+        h5 + h1, h5 + h2, h5 + h3, h5 + h4, h5 + h5, h5 + h6,
+        h6 + h1, h6 + h2, h6 + h3, h6 + h4, h6 + h5, h6 + h6
+    `]: {
+        marginTop: '-1.75rem'
     },
     hr: {
         background: 'var(--hr)',
