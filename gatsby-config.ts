41071b3e5c5036198d399591087bdaacfa4083e9
廖文禧 <1219585136@qq.com>
2023-03-21 17:28:00 +0800

    feat: 样式调整

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -25,7 +25,7 @@ const config: GatsbyConfig = {
                     {
                         resolve: `gatsby-remark-images`,
                         options: {
-                            maxWidth: 590
+                            maxWidth: 630
                         }
                     },
                     {
